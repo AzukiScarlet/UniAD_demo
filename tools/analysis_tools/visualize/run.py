bd147cbbb8313d450c52331be48298c47282d284
AzukiScarlet <200320328@stu.hit.edu.cn>
2024-11-04 14:32:36 +0800

    添加projects路径，不然报错

--- tools/analysis_tools/visualize/run.py
+++ tools/analysis_tools/visualize/run.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+#* 将 projects 模块的路径添加到 PYTHONPATH
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
+
+
 import cv2
 import torch
 import argparse
